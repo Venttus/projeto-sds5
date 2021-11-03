57a36fff9e18916cc9af0a8521a3514fc44116af
Venttus <vinimolinavini@gmail.com>
2021-11-03 20:10:48 -0300

    Static Charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import Footer from "components/Footer";
 import DataTable from "components/DataTable";
 import NavBar from "components/NavBar";
+import Barchart from "components/BarChart";
+import DonutChart from "components/DonutChart";
+
 
 function App() {
   return (
@@ -8,7 +11,25 @@ function App() {
       <NavBar />
       <div className="container">
         
-        <h1 className="text-primary">Óla mundo!</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso %</h5>
+            <Barchart />
+          </div>
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso %</h5>
+            <DonutChart />
+            </div>
+          </div>
+
+          <div className="py-3">
+            <h2 className="text-primary">Todas vendas</h2>
+
+
+          </div>        
 
         <DataTable />
       </div>

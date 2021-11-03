b9c683f3bb372cfdeb73f515f4584db573c571a8
Venttus <vinimolinavini@gmail.com>
2021-11-03 19:36:22 -0300

    Basic static componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Óla mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        
+        <h1 className="text-primary">Óla mundo!</h1>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
-  
+
 }
 
 export default App;
